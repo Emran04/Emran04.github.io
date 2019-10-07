656b437982d33a3a49912c9f738c91f324ea85a7
Emran Hossen <imran04ch@gmail.com>
2019-10-07 19:26:41 +0600

    bio update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,6 +9,11 @@ class About extends Component {
           Edit About component or pages/about.jsx to include your information.
         </h1>
 			<img src={'/img/pp.jpg'} alt="Logo" />
+			<p>Hi, I am Emran Hossen, Fullstack web developer. I build and deliver quality software that helps business succeed. I am consultant, programmer, team lead, software architect and teacher.</p>
+			<p>I am currently working as CEO and Co-Founder at GreenTwigs Software service.</p>
+			<p>I love to working with new technologies. Currently I am working Laravel for creating API or web service, ReactJS for consume API. For deployment I use Digitalocean and aws.</p>
+			<p>Get In Touch
+Email me: emran04ch@gmail.com</p>
       </div>
     );
   }
